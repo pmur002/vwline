9a3b21e41c7a39f2a5695cddba998b960238f336
Paul Murrell <paul@stat.auckland.ac.nz>
2017-02-22 09:10:21 +1300

    changed resolveSpacing() so that spacing is repeated along the length of the line

--- R/spacing.R
+++ R/spacing.R
@@ -9,6 +9,13 @@ resolveSpacing <- function(s, length) {
         ## Assume s is proportions of length
         ss <- s*length
     }
+    ss <- sort(ss)
+    ## Repeat to fill path
+    mss <- max(ss)
+    if (mss < length) {
+        multiple <- (length - mss) %/% mss + 1
+        ss <- c(ss, rep(1:multiple*mss, each=length(ss)) + rep(ss, multiple))
+    }
     ## Trim to path 
     rs <- round(ss, 4)
     subset <- rs >= 0 & rs <= round(length, 4)
